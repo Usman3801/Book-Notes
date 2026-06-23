086c9811bd0b2395e0e8920ae196023a8d1f31ef
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-06-24 00:11:21 +0100

    remove exposed credentials, use env vars

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -10,11 +12,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "books",
-  password: "123-ayomide",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
